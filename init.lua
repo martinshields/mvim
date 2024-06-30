4ec213ecb27bc351f955ba168e3b9dc710367849
martin35 <op6666@gmail.com>
2024-06-29 20:10:14 -0400

    fix treesitter

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Bootstrap lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,15 +17,82 @@ vim.g.mapleader = " "
 
 -- Initialize lazy with dynamic loading of anything in the plugins directory
 require("lazy").setup("plugins", {
-  change_detection = {
-    enabled = true, -- automatically check for config file changes and reload the ui
-    notify = false, -- turn off notifications whenever plugin changes are made
-  },
+	change_detection = {
+		enabled = true, -- automatically check for config file changes and reload the ui
+		notify = false, -- turn off notifications whenever plugin changes are made
+	},
 })
 
 -- These modules are not loaded by lazy
 require("core.options")
 require("core.keymaps")
-require("bufferline").setup {}
-require('mini.animate').setup()
+require("bufferline").setup({})
+require("mini.animate").setup()
 --treesitter
+require("nvim-treesitter.configs").setup({
+	-- -- A list of parser names, or "all" (the five listed parsers should always be installed)
+	-- ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+	ensure_installed = {
+		"bash",
+		-- "c",
+		-- "diff",
+		-- "html",
+		-- "javascript",
+		-- "jsdoc",
+		"json",
+		"jsonc",
+		"lua",
+		"luadoc",
+		-- "luap",
+		"markdown",
+		"markdown_inline",
+		-- "printf",
+		"python",
+		"query",
+		"regex",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"vimdoc",
+		"xml",
+		"yaml",
+	},
+
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+
+	-- List of parsers to ignore installing (or "all")
+	ignore_install = { "javascript" },
+
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+	highlight = {
+		enable = true,
+
+		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+		-- the name of the parser)
+		-- list of language that will be disabled
+		-- disable = { "c", "rust" },
+		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
