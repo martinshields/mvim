0821bbbfc5ff38fa18a39c3f9e2b40d11fb829a3
martin35 <op6666@gmail.com>
2025-02-05 13:21:40 -0500

    last up date

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,140 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			preset = {
+				header = [[
+        
+███╗   ███╗██╗   ██╗██╗███╗   ███╗
+████╗ ████║██║   ██║██║████╗ ████║
+██╔████╔██║██║   ██║██║██╔████╔██║
+██║╚██╔╝██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚═╝ ██║ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝     ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+                                  
+
+                                                             
+
+      ]],
+			},
+		},
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
+		quickfile = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+		styles = {
+			notification = {
+				wo = { wrap = true }, -- Wrap notifications
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>z",
+			function()
+				Snacks.zen()
+			end,
+			desc = "Toggle Zen Mode",
+		},
+		{
+			"<leader>Z",
+			function()
+				Snacks.zen.zoom()
+			end,
+			desc = "Toggle Zoom",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
+		{
+			"<leader>bd",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete Buffer",
+		},
+		{
+			"<leader>cr",
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = "Rename File",
+		},
+		{
+			"<leader>-",
+			function()
+				Snacks.scratch()
+			end,
+			desc = "Toggle Scratch Buffer",
+		},
+		{
+			"<leader>S",
+			function()
+				Snacks.scratch.select()
+			end,
+			desc = "Select Scratch Buffer",
+		},
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Git Browse",
+		},
+		{
+			"<leader>gb",
+			function()
+				Snacks.git.blame_line()
+			end,
+			desc = "Git Blame Line",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.lazygit.log_file()
+			end,
+			desc = "Lazygit Current File History",
+		},
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.lazygit.log()
+			end,
+			desc = "Lazygit Log (cwd)",
+		},
+		{
+			"]]",
+			function()
+				Snacks.words.jump(vim.v.count1)
+			end,
+			desc = "Next Reference",
+			mode = { "n", "t" },
+		},
+		{
+			"[[",
+			function()
+				Snacks.words.jump(-vim.v.count1)
+			end,
+			desc = "Prev Reference",
+			mode = { "n", "t" },
+		},
+	},
+}
